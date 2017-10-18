26c5a4d624f9ce4fa54c4d1093488c66b2cebfd2
Course Admin <noreply@noreply.noreply>
2017-10-18 15:01:06 -0400

    Init hw4 basecode

---
+++ hw4/include/sfish.h
@@ -0,0 +1,9 @@
+#ifndef SFISH_H
+#define SFISH_H
+
+/* Format Strings */
+#define EXEC_NOT_FOUND "sfish: %s: command not found\n"
+#define JOBS_LIST_ITEM "[%d] %s\n"
+#define STRFTIME_RPRMT "%a %b %e, %I:%M%p"
+
+#endif
