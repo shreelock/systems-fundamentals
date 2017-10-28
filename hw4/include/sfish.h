320e7ec1f682909eddf57cab742aaafb544cf9ea
Course Admin <noreply@noreply.noreply>
2017-10-28 16:56:01 -0400

    Add format strings for error handling

--- hw4/include/sfish.h
+++ hw4/include/sfish.h
@@ -5,5 +5,8 @@
 #define EXEC_NOT_FOUND "sfish: %s: command not found\n"
 #define JOBS_LIST_ITEM "[%d] %s\n"
 #define STRFTIME_RPRMT "%a %b %e, %I:%M%p"
+#define BUILTIN_ERROR  "sfish builtin error: %s\n"
+#define SYNTAX_ERROR   "sfish syntax error: %s\n"
+#define EXEC_ERROR     "sfish exec error: %s\n"
 
 #endif
