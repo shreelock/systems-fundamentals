ebb2a7c6d23522fc25ffc53879f3c3141e1105b4
Shree Nath <shreesharma@cs.stonybrook.edu>
2017-11-17 20:28:37 -0500

    hash map implemetation v1

--- hw5/src/cream.c
+++ hw5/src/cream.c
@@ -6,7 +6,46 @@ void queue_free_function1(void *item) {
     free(item);
 }
 
+uint32_t jenkins_hash2(map_key_t map_key) {
+    const uint8_t *key = map_key.key_base;
+    size_t length = map_key.key_len;
+    size_t i = 0;
+    uint32_t hash = 0;
+
+    while (i != length) {
+        hash += key[i++];
+        hash += hash << 10;
+        hash ^= hash >> 6;
+    }
+
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
+}
+
+void map_free_function2(map_key_t key, map_val_t val) {
+    free(key.key_base);
+    free(val.val_base);
+}
+
 int main(int argc, char *argv[]) {
+    hashmap_t* hm = create_map(10, jenkins_hash2, map_free_function2);
+    map_key_t mk;
+    mk.key_len=10;
+    mk.key_base = "key";
+
+    map_val_t mv;
+    mv.val_len=10;
+    mv.val_base= "value";
+    
+    put(hm, mk, mv, false);
+    map_val_t mvt = get(hm, mk);
+    printf("%s", (char*) mvt.val_base);
+    exit(0);
+}
+
+int queue_test(int argc, char *argv[]) {
     queue_t* gh = create_queue();
     enqueue(gh,"1");
 //    enqueue(gh,"2");
--- hw5/src/hashmap.c
+++ hw5/src/hashmap.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include "utils.h"
 
 #define MAP_KEY(base, len) (map_key_t) {.key_base = base, .key_len = len}
@@ -5,14 +6,124 @@
 #define MAP_NODE(key_arg, val_arg, tombstone_arg) (map_node_t) {.key = key_arg, .val = val_arg, .tombstone = tombstone_arg}
 
 hashmap_t *create_map(uint32_t capacity, hash_func_f hash_function, destructor_f destroy_function) {
-    return NULL;
+    struct hashmap_t *hmap = (hashmap_t*) calloc(1, sizeof(hashmap_t));
+    hmap->nodes = (map_node_t*)calloc(capacity, sizeof(map_node_t));
+    hmap->capacity = capacity;
+    hmap->size = 0;
+    hmap->hash_function = hash_function;
+    hmap->destroy_function = destroy_function;
+    hmap->num_readers = 0;
+    hmap->invalid = false;
+    if(pthread_mutex_init(&hmap->write_lock, NULL)!=0) return NULL;
+    if(pthread_mutex_init(&hmap->fields_lock, NULL)!=0) return NULL;
+    return hmap;
 }
 
-bool put(hashmap_t *self, map_key_t key, map_val_t val, bool force) {
+bool put(hashmap_t *self, map_key_t ikey, map_val_t ival, bool force) {
+    int index = get_index(self, ikey);
+    if(self->capacity > self->size)
+        force = false;
+
+    if(self->size == self->capacity && force == false) {
+        errno = ENOMEM;
+        return false;
+    }
+
+
+    map_node_t *foundnode = self->nodes + index;
+    map_key_t skey = foundnode->key;
+
+    // If we reached capacity, overwriting the index value
+    if(self->capacity == self->size && force == true){
+        foundnode->key = ikey;
+        foundnode->val = ival;
+        foundnode->tombstone = false;
+        return true;
+    }
+
+    if(skey.key_base==NULL) {
+        foundnode->key = ikey;
+        foundnode->val = ival;
+        foundnode->tombstone = false;
+        self->size++;
+        return true;
+    }
+    // if keys are same, update.
+    else if(skey.key_base == ikey.key_base && skey.key_len == ikey.key_len) {
+        foundnode->val = ival;
+        foundnode->tombstone = false;
+        return true;
+    }
+        // Else, check for consecutive locations in the hashmap.
+    else {
+        int oldindex = index;
+        while (index++ % self->capacity != oldindex){
+            map_node_t *node = self->nodes + index;
+            map_key_t tkey = node->key;
+
+            //If keys are same for some next index, overwrite.
+            if(tkey.key_base == ikey.key_base && tkey.key_len == ikey.key_len) {
+                node->val = ival;
+                node->tombstone=false;
+                return true;
+            }
+                // Else if Key itself is null, i.e. no item is present,
+                // then we found an empty space in the later spaces and
+                // we are assigning it to the key
+            else if (tkey.key_base == NULL) {
+                node->val = ival;
+                node->tombstone=false;
+                self->size++;
+                return true;
+            }
+        }
+        // we traversed the whole array for that key, and it was no where to be
+        // found. return the null value.
+    }
     return false;
 }
 
-map_val_t get(hashmap_t *self, map_key_t key) {
+map_val_t get(hashmap_t *self, map_key_t ikey) {
+    int index = get_index(self, ikey);
+    map_node_t *foundnode = self->nodes + index;
+    map_key_t skey = foundnode->key;
+    map_val_t sval = foundnode->val;
+    // if keys are same, check if val is not null and return accordingly.
+    if(skey.key_base == ikey.key_base && skey.key_len == ikey.key_len) {
+        if(sval.val_base != NULL)
+            return MAP_VAL(sval.val_base, sval.val_len);
+        else
+            return MAP_VAL(NULL, 0);
+    }
+        // Else, check for consecutive locations in the hashmap.
+    else {
+        int oldindex = index;
+        while (index++ % self->capacity != oldindex){
+            map_node_t *node = self->nodes + index;
+            // do nothing if we found a tombstone on a node.
+            if(node->tombstone)
+                continue;
+
+            map_key_t tkey = node->key;
+            map_val_t tval = node->val;
+
+            //If keys are same, check if val is not null and return accordingly.
+            if(tkey.key_base == ikey.key_base && tkey.key_len == ikey.key_len) {
+                if(tval.val_base!=NULL)
+                    return MAP_VAL(tval.val_base, tval.val_len);
+                else
+                    return MAP_VAL(NULL, 0);
+            }
+                // Else if Key itself is null, i.e. no item is present,
+                // then we found an empty space in the later spaces and
+                // we couldn't find out requested key.
+            else if (tkey.key_base == NULL) {
+                return MAP_VAL(NULL, 0);
+            }
+        }
+        // we traversed the whole array for that key, and it was no where to be
+        // found. return the null value.
+    }
     return MAP_VAL(NULL, 0);
 }
 
